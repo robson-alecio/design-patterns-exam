c83f8405639dd9feac72cecec428f9b7eb899311
willgkm <wkuviatkovski@gmail.com>
2022-06-09 20:12:31 -0300

    Decorator funcionando

--- src/main/java/exam/designpatterns/decorator/TudoMaiusculaDecorator.java
+++ src/main/java/exam/designpatterns/decorator/TudoMaiusculaDecorator.java
@@ -1,7 +1,13 @@
 package exam.designpatterns.decorator;
 
 public class TudoMaiusculaDecorator extends TextoDecorator {
+	
 	public TudoMaiusculaDecorator(TextoDecorator decorator) {
 		super(decorator);
+	}
+
+	@Override
+	public String processar(String texto) {
+		return super.processar(texto).toUpperCase();
 	}
 }
