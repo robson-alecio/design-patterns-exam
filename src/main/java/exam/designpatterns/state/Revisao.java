d077bcddcaa258bc9c1fce1217a04137c945ec4c
Enzo Maximiliano Rodrigues Lemes <73507992+enzomaximilinao@users.noreply.github.com>
2022-06-12 20:52:31 -0300

    Update Revisao.java

--- src/main/java/exam/designpatterns/state/Revisao.java
+++ src/main/java/exam/designpatterns/state/Revisao.java
@@ -1,16 +1,14 @@
-package exam.designpatterns.state;
+package designpatterns.state;
 
 public class Revisao extends State {
 
 	public Revisao(Publicacao publicacao) {
 		super(publicacao);
 	}
-
 	@Override
 	protected boolean editar(String texto) {
 		return false;
 	}
-
 	@Override
 	public State proximoState() {
 		return null;
